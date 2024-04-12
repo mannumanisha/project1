1c8c90e75159bce8f571a92a58036c0d89445f9e
manisha bajjuri <153098860+mannumanisha@users.noreply.github.com>
2024-04-12 16:22:06 +0530

    Update main.tf

--- project1/battleships/main.tf
+++ project1/battleships/main.tf
@@ -1,41 +1,46 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_instance" "example" {
-  ami           = "ami-080e1f13689e07408"  # Replace with Ubuntu AMI ID
+  ami           = "ami-080e1f13689e07408"
   instance_type = "t2.micro"
-  key_name      = "project"   # Replace with your key pair name
+  key_name      = "project"
   security_groups = [
-    "my-sec"          # Replace with your security group name
+    "my-sec"
   ]
   tags = {
     Name = "Ubuntu Instance"
   }
 }
+
 resource "aws_ebs_volume" "example_volume" {
-  availability_zone = "us-east-1a"  # Specify the availability zone where your instance is located
-  size              = 8             # Specify the size of the volume in gigabytes
+  availability_zone = "us-east-1a"
+  size              = 8
 }
+
 resource "aws_volume_attachment" "example_attachment" {
-  device_name = "/prod/xvdf"             # Specify the device name for the volume
+  device_name = "/dev/xvdf"  # Changed device name for Ubuntu
   volume_id   = aws_ebs_volume.example_volume.id
-  instance_id = aws_instance.example.id  # Specify the instance ID
+  instance_id = aws_instance.example.id
 }
- provisioner "remote-exec" {
+
+resource "null_resource" "install_tools" {
+  provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get install -y git"
-"git=2.33.1-0ubuntu0.20.04.2"
+      "sudo apt-get install -y git unzip",
+      "sudo unzip terraform_1.0.0_linux_amd64.zip -d /usr/local/bin/"  # Adjusted unzip command for Ubuntu
     ]
 
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("C:\Users\Manisha\Downloads\project.pem")  # Replace with the path to your private key file
-      host        = self.public_ip
+      private_key = file("C:\Users\Manisha\Downloads\project.pem")
+      host        = aws_instance.example.public_ip
     }
   }
-
+}
 variable "aws_access_key" {
   description = "AWS Access Key ID"
 }
